609f389c148fe2e44f8a708f3d205db1c38dafc3
Mirko Predosin <mpredosin@gmail.com>
2020-01-05 16:05:39 -0800

    Add thrust to lander.py

--- lander.py
+++ lander.py
@@ -24,19 +24,23 @@ def init():
     lander = pygame.Rect(-5, landerHeight, 10, 10)
 
 
-screenWidth = 800
-screenHeight = 800
+screenWidth = 1200
+screenHeight = 720
 
-worldWidth = float(200)
-worldHeight = float(200)
+worldWidth = float(screenWidth / 2)
+worldHeight = float(screenHeight / 2)
 
-gravity = -9.81  # gravity in msec
+gravity = -5  # moon gravity
+thrust = -gravity * 3
+
+currentThrust = 0.0
 
 init()
 pygame.init()
 pygame.font.init()
 pygame.display.set_caption("Lander")
 font = pygame.font.Font("freesansbold.ttf", 20)
+fontLineSize = font.get_linesize() / scaleY
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 previousTime = pygame.time.get_ticks() / 1000
@@ -86,8 +90,9 @@ def render():
     #print("current",currentTime,"diff",deltaTime)
     previousTime = currentTime
 
-    velocity = previousVelocity + gravity * deltaTime
-    distance = previousVelocity * deltaTime + gravity * deltaTime * deltaTime / 2
+    acceleration = gravity + currentThrust
+    velocity = previousVelocity + acceleration * deltaTime
+    distance = previousVelocity * deltaTime + acceleration * deltaTime * deltaTime / 2
     landerHeight = landerHeight + distance
 
     #print("v0",previousVelocity,"v",velocity,"s",distance,"h",landerHeight)
@@ -97,6 +102,25 @@ def render():
     lander.y = landerHeight
 
     screen.fill(black)
+
+    leftMargin = 100
+    altitudeStr = "Altitude: " + str(lander.y - lander.height)
+    altitudeText = font.render(altitudeStr, True, white)
+    screen.blit(altitudeText,
+                translate(worldWidth / 2 - leftMargin, worldHeight))
+
+    horizontalSpeedStr = "Horizontal Speed: " + str(int(round(0.0)))
+    horizontalSpeedText = font.render(horizontalSpeedStr, True, white)
+    screen.blit(
+        horizontalSpeedText,
+        translate(worldWidth / 2 - leftMargin, worldHeight - fontLineSize))
+
+    verticalSpeedStr = "Vertical Speed: " + str(int(round(velocity)))
+    verticalSpeedText = font.render(verticalSpeedStr, True, white)
+    screen.blit(
+        verticalSpeedText,
+        translate(worldWidth / 2 - leftMargin, worldHeight - fontLineSize * 2))
+
     pygame.draw.rect(screen, white, translateRect(lander))
     pygame.display.update()
 
@@ -118,15 +142,12 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             if event.key == pygame.K_s:
-                lander.move_ip(0, -1)
-            if event.key == pygame.K_w:
-                lander.move_ip(0, 1)
-            if event.key == pygame.K_a:
-                lander.move_ip(-1, 0)
-            if event.key == pygame.K_d:
-                lander.move_ip(1, 0)
+                currentThrust = thrust
             if event.key == pygame.K_r:
                 init()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_s:
+                currentThrust = 0.0
 
     render()
 
